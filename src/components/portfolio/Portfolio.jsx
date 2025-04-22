7bfbe0b502b2eaafaac2f2f6f34bca2732029ce5
Rajkumar Verma <Rv782975@gmail.com>
2025-04-23 02:53:43 +0530

    new project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import IMG1 from '../../assets/Gemini.png';
-import IMG2 from '../../assets/LandingPage.png';
-import IMG3 from '../../assets/todo.png';
-import IMG4 from '../../assets/Amazon.png';
-import IMG5 from '../../assets/news.png';
-import IMG6 from '../../assets/math.png';
+import IMG2 from '../../assets/yt.png';
+import IMG3 from '../../assets/Insta.png';
+import IMG4 from '../../assets/LandingPage.png';
 
 import './portfolio.css';
 
 const Portfolio = () => {
   const soloProjects = [
+    {
+      id: 3,
+      title: 'Instagram Clone',
+      img: IMG3,
+      description: 'A fully responsive Instagram clone built with React.js, featuring post creation, user authentication, and real-time updates.',
+      technologies: 'React.js | Mongodb | Tailwind',
+      link: 'https://instaclone-nn56.onrender.com/',
+      github: 'https://github.com/PrinceRaj82/InstaClone'
+    },
+    {
+      id: 2,
+      title: 'YT Playlist Anlyser',
+      img: IMG2,
+      description:
+        ' YouTube Playlist Analyzer that calculates total duration, visualizes video data with charts, supports dark mode, and enables sharing or embedding results — built with React, Vite, and Tailwind CSS.',
+      technologies: 'React | Typescript | Tailwind',
+      link: 'https://ytplaylistanlyser.vercel.app/',
+      github: 'https://github.com/PrinceRaj82/YTPLAYLISTANLYSER',
+    },
     {
       id: 1,
       title: 'Gemini Functional Clone',
@@ -20,54 +37,14 @@ const Portfolio = () => {
       github: 'https://github.com/PrinceRaj82/GeminiClone',
     },
     {
-      id: 2,
-      title: 'YT Playlist Anlyser',
-      img: IMG4,
-      description:
-        ' YouTube Playlist Analyzer that calculates total duration, visualizes video data with charts, supports dark mode, and enables sharing or embedding results — built with React, Vite, and Tailwind CSS.'
-      technologies: 'React | Typescript | Tailwind',
-      link: 'https://ytplaylistanlyser.vercel.app/',
-      github: 'https://github.com/PrinceRaj82/YTPLAYLISTANLYSER',
-    },
-    // {
-    //   id: 3,
-    //   title: 'YoutubeClone',
-    //   img: IMG3,
-    //   description:
-    //     'Fully responsive interactive website built based on Figma design',
-    //   technologies: 'JavaScript | CSS',
-    //   link: 'https://meri-mg.github.io/shelter/pages/main/index.html',
-    //   github: 'https://github.com/Meri-MG/shelter',
-    // },
-    {
       id: 4,
       title: 'Menu page',
-      img: IMG2,
+      img: IMG4,
       description: 'An interactive menu using HTML, CSS, and JavaScript with smooth navigation and a user-friendly design for easy browsing',
       technologies: 'JavaScript | HTML | CSS',
       link: 'https://menu-landingpge.netlify.app/',
       github: 'https://github.com/PrinceRaj82/MenuPage',
     },
-    // {
-    //   id: 5,
-    //   title: 'World News',
-    //   img: IMG5,
-    //   description:
-    //     'Fully responsive interactive website built based on Adobe XD design',
-    //   technologies: 'JavaScript | CSS',
-    //   link: 'https://meri-mg.github.io/Unilab-world-news/',
-    //   github: 'https://github.com/Meri-MG/Unilab-world-news',
-    // },
-    // {
-    //   id: 6,
-    //   title: 'Math Resource',
-    //   img: IMG6,
-    //   description:
-    //     'Real-world group project which is still in progress and will provide educational platform for future young developers',
-    //   technologies: 'JavaScript | Scss | Python',
-    //   link: 'https://lukinoo.github.io/math-resource/',
-    //   github: 'https://github.com/lukinoo/math-resource',
-    // },
   ];
 
   return (
